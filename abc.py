e5c8c4faf21305fbdb29994e98abc1308cdd566a
Shrushti Jadhav <67223703+shrushtijadhav@users.noreply.github.com>
2022-02-22 21:08:09 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -3,3 +3,10 @@ radius = float(input("Enter the radius"))
 print(radius)
 area = PI * radius * radius
 print(area)
+
+
+# WAP to accept a filename from the user and print the extension of that:
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
